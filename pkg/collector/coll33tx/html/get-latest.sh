2cdbf24e5d10a59a2d4ce56d50150149fc68ba7c
florinutz <florinutz@gmail.com>
2019-02-13 17:28:25 +0100

    preparing tests for the list page

--- pkg/collector/coll33tx/html/get-latest.sh
+++ pkg/collector/coll33tx/html/get-latest.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # keep these base64d so they don't show up in some web search
-content=$(curl -sL https://1337x.to/torrent/3570061/House-Party-1990-WEBRip-1080p-YTS-YIFY/)
-echo ${content} | base64 > detail
+echo $(curl -sL https://1337x.to/torrent/3570061/House-Party-1990-WEBRip-1080p-YTS-YIFY/) | base64 > detail
 echo "fetched detail html"
 
-content=$(curl -sL https://1337x.to/popular-movies)
-echo ${content} | base64 > list
+echo $(curl -sL https://1337x.to/popular-movies) | base64 > list
 echo "fetched list html"
+
+echo $(curl -sL https://1337x.to/search/dvdrip/1/) | base64 > list_pagination
+echo "fetched list with pagination html"
