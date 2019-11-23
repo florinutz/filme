ef1d63beb257bec2b56e224550a70b95b70e8b3f
florin <florinutz@gmail.com>
2019-11-23 16:03:09 +0100

    fixing an infinite recursive call detected by from staticcheck

--- pkg/config/value/debug_level.go
+++ pkg/config/value/debug_level.go
@@ -2,10 +2,8 @@ package value
 
 import "github.com/sirupsen/logrus"
 
-type DebugLevelValue logrus.Level
-
-func (v *DebugLevelValue) String() string {
-	return v.String()
+type DebugLevelValue struct {
+	logrus.Level
 }
 
 func (v *DebugLevelValue) Set(lvl string) error {
@@ -13,7 +11,7 @@ func (v *DebugLevelValue) Set(lvl string) error {
 	if err != nil {
 		return err
 	}
-	*v = DebugLevelValue(level)
+	v.Level = level
 
 	return nil
 }
