7a8f83b54c9339344a9b2f6f44dec8dcd59a025a
Florin <florinutz@users.noreply.github.com>
2019-11-26 16:02:35 +0100

    cleaner search fields logging (#7)

--- pkg/filme/search.go
+++ pkg/filme/search.go
@@ -1,26 +1,14 @@
 package filme
 
 import (
-	"fmt"
-
 	"github.com/florinutz/filme/pkg/filme/l33tx/list"
 	filt "github.com/florinutz/filme/pkg/filme/l33tx/list/filter"
-	input2 "github.com/florinutz/filme/pkg/filme/l33tx/list/input"
+	"github.com/florinutz/filme/pkg/filme/l33tx/list/input"
 )
 
-func (f *Filme) Search(goIntoDetails bool, inputs input2.ListingInput, filters filt.Filter) error {
-	log := *f.Log.WithFields(map[string]interface{}{
-		"search_inputs_encoding": inputs.Encoding,
-		"search_inputs_category": inputs.Category,
-		"search_inputs_sort":     inputs.Sort,
-		"search_inputs_search":   inputs.Search,
-		"search_inputs_url":      inputs.Url,
-		"with_details":           goIntoDetails,
-		"filter_max_items":       fmt.Sprint(filters.MaxItems),
-		"filter_leechers":        fmt.Sprint(filters.Leechers),
-		"filter_seeders":         fmt.Sprint(filters.Seeders),
-		"filter_size":            fmt.Sprint(filters.Size),
-	})
+func (f *Filme) Search(goIntoDetails bool, inputs input.ListingInput, filters filt.Filter) error {
+	logFields := localLogFields(inputs, goIntoDetails, filters)
+	log := *f.Log.WithFields(logFields)
 
 	ls := list.NewList(inputs, filters, f.Out, log)
 
@@ -44,4 +32,29 @@ func (f *Filme) Search(goIntoDetails bool, inputs input2.ListingInput, filters f
 	col.Wait()
 
 	return nil
+}
+
+func localLogFields(inputs input.ListingInput, goIntoDetails bool, filters filt.Filter) (result map[string]interface{}) {
+	result = map[string]interface{}{
+		"search_inputs_sort": inputs.Sort,
+		"with_details":       goIntoDetails,
+	}
+	if inputs.Encoding != nil && *inputs.Encoding != 0 {
+		result["search_inputs_encoding"] = *inputs.Encoding
+	}
+	if inputs.Category != nil && *inputs.Category != 0 {
+		result["search_inputs_category"] = *inputs.Category
+	}
+	if inputs.Search != "" {
+		result["search"] = inputs.Search
+	}
+	if inputs.Url != nil {
+		result["search_inputs_url"] = inputs.Url.String()
+	}
+
+	for key, v := range filters.GetLogFields() {
+		result[key] = v
+	}
+
+	return
 }
