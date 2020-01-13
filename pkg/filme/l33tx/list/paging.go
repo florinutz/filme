e131f7482c5a804e3e15dff3b4ec201bf454c90e
florin <florinutz@gmail.com>
2020-01-13 11:36:12 +0100

    added the paging support

---
+++ pkg/filme/l33tx/list/paging.go
@@ -0,0 +1,54 @@
+package list
+
+type Paging struct {
+	filterLow    int
+	filterHigh   int
+	limitLow     int
+	limitHigh    int
+	itemsPerPage int
+	pagesToCrawl []int
+}
+
+// getNextPages returns the range of pages that should be scheduled for parsing.
+// Should only be called after a page was crawled.
+func (p Paging) getNextPages(wantedItems int) (pages []int) {
+	if p.filterLow > p.limitHigh {
+		return
+	}
+
+	wantedPages := (wantedItems-1)/p.itemsPerPage + 1
+
+	if p.limitHigh < wantedPages+1 {
+		wantedPages = p.limitHigh - 1
+	}
+
+	bottom := p.limitLow
+	top := bottom + wantedPages
+
+	if p.filterLow > bottom {
+		bottom = p.filterLow
+		top = bottom + wantedPages - 1
+	}
+
+	if p.filterHigh > 0 && p.filterHigh < top {
+		top = p.filterHigh
+	}
+
+	if bottom > top {
+		return
+	}
+
+	for i := bottom; i <= top; i++ {
+		pages = append(pages, i)
+	}
+
+	return
+}
+
+func (p Paging) pageIsValid(pageNo, wantedItems int) bool {
+	neededPages := p.getNextPages(wantedItems)
+	if len(neededPages) == 0 {
+		return false
+	}
+	return pageNo >= neededPages[0] && pageNo <= neededPages[len(neededPages)-1]
+}
